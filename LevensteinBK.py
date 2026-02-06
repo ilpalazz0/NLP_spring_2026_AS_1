fbfb14aed5b99f387be9de2626e294f87f73876c
Mammadov Elmar <67194981+elmarMamedov485@users.noreply.github.com>
2026-02-06 17:46:59 +0400

    updated file

--- LevensteinBK.py
+++ LevensteinBK.py
@@ -1,4 +1,3 @@
-import KeyboardConfusionMatrix
 import numpy as np
 from collections import deque
 
@@ -23,11 +22,11 @@ class BKtree:
         else:
             node = Node(label)
             current = self.root
-            distance = WeightedLevenstein(label, current.label)
+            distance = Levenstein(label, current.label)
 
             while distance in current.children.keys():
                 current = current.children.get(distance)
-                distance = WeightedLevenstein(label, current.label)
+                distance = Levenstein(label, current.label)
             current.children[distance] = node
     
     def search(self, label, max_dist):
@@ -36,7 +35,7 @@ class BKtree:
 
         while len(candidates) > 0:
             node = candidates.popleft()
-            distance = WeightedLevenstein(label, node.label)
+            distance = Levenstein(label, node.label)
 
             if(distance <= max_dist):
                 found.append(node)
@@ -64,27 +63,6 @@ def Levenstein(a, b):
             insertion = lev[i - 1, j] + 1
             deletion = lev[i, j - 1] + 1
             substitution = lev[i-1, j-1] + (1 if (a[i-1] != b[j-1])  else 0)
-            lev[i, j] = min(insertion, deletion, substitution)
-    
-    return lev[n, m]
-
-def WeightedLevenstein(a, b):
-    n = len(a)
-    m = len(b)
-
-    lev = np.zeros((n+1, m+1))
-
-    for i in range (0, n+1):
-        lev[i, 0] = i
-
-    for i in range (0, m+1):
-        lev[0, i] = i
-
-    for i in range (1, n+1):
-        for j in range (1, m+1):
-            insertion = lev[i - 1, j] + 1
-            deletion = lev[i, j - 1] + 1
-            substitution = lev[i-1, j-1] + (KeyboardConfusionMatrix.matrix_cost[a[i-1]][b[j-1]] if ((a[i-1] != b[j-1])) else 0)
             lev[i, j] = min(insertion, deletion, substitution)
     
     return lev[n, m]
